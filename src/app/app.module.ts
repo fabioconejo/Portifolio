8bae3c9c0295393d09f2bd057c64425cd597cd0c
fabioconejo <fabio.conejo@hotmail.com>
2021-05-12 17:26:37 -0300

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, MenuComponent],
+  declarations: [AppComponent, MenuComponent, InicioComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/inicio/inicio.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}

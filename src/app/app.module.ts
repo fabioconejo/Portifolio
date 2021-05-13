fa22e46468e0f9f8821ad3cec24bc69c4ebd97f4
fabioconejo <fabio.conejo@hotmail.com>
2021-05-13 00:54:32 -0300

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, MenuComponent, InicioComponent],
+  declarations: [AppComponent, MenuComponent, InicioComponent, SobreComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/sobre/sobre.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sobre',
+  templateUrl: './sobre.component.html',
+  styleUrls: ['./sobre.component.css']
+})
+export class SobreComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
